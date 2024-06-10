d9ba47a02084e1b9a6a61e7d25f758c4328c6e92
Tom Hinkle <tmhinkle@gmail.com>
2024-06-10 10:11:44 -0400

    ++link to google doc

--- ABOUT.js
+++ ABOUT.js
@@ -2,5 +2,6 @@
  * This code lives here:
  * https://github.com/thinkle-iacs/onboarding-offer-letters-sheet
  *
+ * Documentation lives here: https://docs.google.com/document/d/13ICnmplrh8hzIDU4FDxCAOgxEx8a2le6pRc3WQnGacQ/edit
  * Managed with clasp
  */
